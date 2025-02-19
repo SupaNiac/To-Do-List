0d798c6e47cf39259ccb1f1cfab444fc740b4626
Luiz Henrique <luiz04faria@gmail.com>
2025-02-18 22:43:39 -0300

    Constructors, methods and etc.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entities.Task;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		Task task = new Task();
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Task> task1 = new ArrayList<>();
+		
+		System.out.println("Add a task: ");
+		task.setTask(sc.next());
+		System.out.println(task.getTask());
+		
+		
+		sc.close();
 	}
 
 }
--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,11 +1,25 @@
 package entities;
 
 public class Task {
+	
+		private String task;
+		
+		public Task(String task) {
+			this.task = task;
+		}
 
-	
-	public String Task(Task task) {
-		return this.Task(task);
+		public String getTask() {
+			return task;
+		}
+
+		public void setTask(String task) {
+			this.task = task;
+		}
+		
+		@Override
+		public String toString() {
+			return "Task: " + task;
+		}
+
 	}
-	
-	
-	}
+
