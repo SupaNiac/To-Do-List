dcde8405d94499c45464a5dd7e5e2235885851bd
Luiz Henrique <luiz04faria@gmail.com>
2025-02-19 22:46:33 -0300

    Add and remove options

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,6 +29,32 @@ public class Program {
 			for (Task t : taskList) {
 				System.out.println(t);
 			}
+			
+			System.out.println("Do you want to add more tasks or remove a task? [A/R]");
+			char ra = sc.next().charAt(0);
+			
+			if (ra == 'a') {
+				while (!INPUT.equalsIgnoreCase("Quit")) {
+					System.out.print("Type a task (or 'Quit to exit): ");
+					INPUT = sc.nextLine();
+					
+					if (!INPUT.equalsIgnoreCase("Quit")) {
+						Task task = new Task(INPUT);
+						taskList.add(task);
+						System.out.println("Task added: " + task);
+					}
+					}
+			}
+			else if (ra == 'r') {
+				
+				System.out.println("Your tasks: ");
+				for (int i = 0; i < taskList.size(); i++) {
+					System.out.println((i + 1) + "-" + taskList.get(i).getDescription());
+				}	
+				System.out.print("Type the number of the task you want to remove: ");
+				int number = sc.nextInt();
+			}
+					
 		
 		sc.close();
 	}
